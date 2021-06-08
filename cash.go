ff9b6fe0d62e494e04ed1648fc581ef5316e3040
Bublia <71136227+Bublia@users.noreply.github.com>
2021-06-08 17:30:30 +0300

    Update cash.go

--- cash.go
+++ cash.go
@@ -2,5 +2,36 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello World")
+    
+    fmt.Println("How much dollars do you have?: ")
+    var cash float32
+    var counter int
+    counter = 0
+    fmt.Scan(&cash)
+   
+  
+    temp := (int)(cash * 100)
+
+    for temp >= 25 {
+		temp = temp - 25
+        counter = counter + 1
+	}
+
+    for temp >= 10 {
+		temp = temp - 10
+        counter = counter + 1
+	}
+
+    for temp >= 5 {
+		temp = temp - 5
+        counter = counter + 1
+	}
+
+    for temp >= 1 {
+		temp = temp - 1
+        counter = counter + 1
+	}
+    
+    fmt.Println("That's your coins count:")
+    fmt.Println(counter)
 }
