a399e34099292824c8e4740390fa39604587f4f1
JonasSpa <72869248+JonasSpa@users.noreply.github.com>
2021-06-16 16:45:27 +0300

    cash :DDd
    Jonas
    Geros Vasaros!!!

--- cash.cs
+++ cash.cs
@@ -1,6 +1,37 @@
 using System;
 class HelloWorld {
   static void Main() {
-    Console.WriteLine("Hello World");
+    string userInputString = Console.ReadLine();
+    float userInputFloat = float.Parse(userInputString);
+    int userInputInt = (int)Math.Round(userInputFloat * 100);
+    int counter = 0;
+    while(userInputInt > 0)
+    {
+        if(userInputInt - 25 >= 0)
+        {
+            userInputInt = userInputInt - 25;
+            counter++;
+        }
+        else if (userInputInt - 10 >= 0)
+        {
+            userInputInt = userInputInt - 10;
+            counter++;
+        }
+        else if (userInputInt - 5 >= 0)
+        {
+            userInputInt = userInputInt - 5;
+            counter++;
+        }
+        else if (userInputInt - 1 >=0)
+        {
+            userInputInt = userInputInt - 1;
+            counter++;
+        }
+    }
+    
+    
+    Console.WriteLine(counter);
+  }
+}
   }
 }
